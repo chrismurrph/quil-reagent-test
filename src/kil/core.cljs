228548203e38cdecda99d194c84cc48354564d5c
chris <chris@strandz.org>
2015-10-27 08:26:29 +1100

    one name 'foo' - hides that there are really two namable things - a canvas id and a sketch function name

--- src/kil/core.cljs
+++ src/kil/core.cljs
@@ -8,30 +8,30 @@
 (def w 400)
 (def h 400)
 
-(defn setup []
+(defn setup-quil-canvas []
   {:t 1})
 
-(defn update [state]
+(defn update-quil-canvas [state]
   (update-in state [:t] inc))
 
-(defn draw [state]
+(defn draw-quil-canvas [state]
   (q/background 255)
   (q/fill 0)
   (q/ellipse (rem (:t state) w) 46 55 55))
 
-(q/defsketch foo
-  :setup  setup
-  :update update
-  :draw   draw
-  :host "foo"
+(q/defsketch quil-sketch-fn
+  :setup  setup-quil-canvas
+  :update update-quil-canvas
+  :draw   draw-quil-canvas
+  :host "host-canvas-id"
   :no-start true
   :middleware [m/fun-mode]
   :size [w h])
 
 (defn hello-world []
   (reagent/create-class
-    {:reagent-render (fn [] [:canvas#foo {:width w :height h}])
-     :component-did-mount foo}))
+    {:reagent-render (fn [] [:canvas#host-canvas-id {:width w :height h}])
+     :component-did-mount quil-sketch-fn}))
 
 (reagent/render-component
   [hello-world]
